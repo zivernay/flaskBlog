b2a61dc77e3dc75858d8fdec74e925fbbcc277c5
zivernay <ad.zivanai@outlook.com>
2021-03-11 16:52:41 +0300

    added templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask  #flaks class 
+from flask import render_template # Jinja 2  engine for rendering teplates
+app = Flask(__name__) 
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
-@app.route("/")
+@app.route("/") #setting up the routes/ url which run the function
+@app.route("/home")
 def hello():
-    return "Hello World"
+    return render_template("home.html", posts = posts)
 
-@app.route("/cool")
+@app.route("/about")
 def html():
-    return '''
-    <html>
-    <body style="background-color:black; color:blue"><h1>Cool</h1></body>
-    </html>'''
+    return render_template("about.html", title = "Doma")
 
 
-""" if __name__ == "__main__":
-    app.run(debug=True) """
+if __name__ == "__main__":
+    app.run(debug=True)
