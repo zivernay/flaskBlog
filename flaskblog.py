92ad2ab9758a2c734892abcdc2fa0ec1f935b393
zivernay <ad.zivanai@outlook.com>
2021-03-16 17:27:24 +0300

    databae models

--- flaskblog.py
+++ flaskblog.py
@@ -4,9 +4,42 @@ from flask import render_template # Jinja 2  engine for rendering teplates
 from flask import flash # diplays one time messages
 from flask import url_for # creates links for routing
 from forms import RegistrationForm, Login, MoreInfo
+from flask_sqlalchemy import SQLAlchemy #import the SQL class
+
+from datetime import datetime
 app = Flask(__name__) 
 
 app.config['SECRET_KEY'] = '1'
+app.config['SQLACHEMY_DATABASE_URL'] = "sqlite:///site.db"
+db = SQLAlchemy(app) #instatiating the db
+
+class User(db.Model):
+    id = db.Column(db.Integer,nullable=False, primary_key=True, unique=True)
+    username = db.Column(db.String(128), unique=True, nullable=False)
+    fname = db.Column(db.String(30), nullable=False)
+    lname = db.Column(db.String(30), nullable=False)
+    image_file  = db.Column(db.String(20), default="default.jpg")
+    password = db.Column(db.String(60), nullable=True)
+
+    posts = db.relationship("Post", backref="author", lazy = True)
+
+    def __repr__(self):
+        return f"User({self.username}, {self.id}, {self.image_file})"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    title = db.Column(db.String(20), nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return f"Post({self.id}, {self.title}, {self.date_posted})"
+
+
+
 posts = [
     {
         'author': 'AD Zivanai',
